ef88299d63428a55ebbefebe380e2baf62ae418b
rkok3210 <32310566+rkok3210@users.noreply.github.com>
2018-11-14 21:07:31 +0000

    App.py file from tonight

--- app.py
+++ app.py
@@ -2,15 +2,32 @@ from flask import Flask
 from flask import Markup
 from flask import Flask
 from flask import render_template
+import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/')
-def barchart():
-    labels = ["Associate degree", "Bachelor's degree", "I never completed any formal education", "Master's degree", "Other doctoral degree", "Primary/elementary school", "Professional degree (JD, MD, etc.)", "Secondary school", "Some college/university study without earning a degree"]
-    values = [3.1, 46.1, 0.7, 22.6, 2.3, 1.7, 1.5, 9.5,12.4]
-    return render_template('chart.html', values=values, labels=labels)
+@app.route('/test')
+def barchart1():
 
-if __name__ == '__main__':
-	app.run()
+    data = pd.read_csv('survey_results_public.csv')
+
+    summary = data['FormalEducation'].value_counts()
+    values = []
+    for i in summary:
+        values.append(i)
+   
+    print(values)
+
+
+    labels = ["Associate degree", "Bachelor's degree", "I never completed any formal education", "Master's degree",
+          "Other doctoral degree", "Primary/elementary school", "Professional degree (JD, MD, etc.)",
+          "Secondary school", "Some college/university study without earning a degree"]
+
+    return render_template('chart.html', values=values, labels=labels)
 	
+	
+if __name__ == '__main__':
+	app.run(debug=True)
+	
+
+
