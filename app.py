c2f3501581cd638420d2682fc384a25eba2c8cfd
rkok3210 <32310566+rkok3210@users.noreply.github.com>
2018-11-16 22:32:46 +0000

    latest app.py file

--- app.py
+++ app.py
@@ -6,28 +6,31 @@ import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/test')
+@app.route('/chart')
 def barchart1():
 
-    data = pd.read_csv('survey_results_public.csv')
+    data = pd.read_csv('survey_results_public.csv', usecols=['FormalEducation'],encoding='latin-1', low_memory=False)
 
     summary = data['FormalEducation'].value_counts()
+
     values = []
     for i in summary:
         values.append(i)
-   
-    print(values)
 
+    values.sort()
 
-    labels = ["Associate degree", "Bachelor's degree", "I never completed any formal education", "Master's degree",
-          "Other doctoral degree", "Primary/elementary school", "Professional degree (JD, MD, etc.)",
-          "Secondary school", "Some college/university study without earning a degree"]
+         ##print(len(summary.index)) -- counted total row - in theory we should output this to the page less the NAs
+
+    ## apostrophes are rendering wrong - this is not the right order-chart is orderd by size
+    ## it doesn't seem to count NA?  - value counts must ignore it
+    ## I put in the labels in the order of size of the results. 
+    labels = ['I never completed any formal education','Professional degree','Primary/elementary school','Doctoral degree', 'Associate degree','Secondary school','Some college/university study without earning a degree','Master\'s degree', 'Bachelor\'s degree']
+
 
     return render_template('chart.html', values=values, labels=labels)
-	
-	
+
+
 if __name__ == '__main__':
 	app.run(debug=True)
-	
 
 
